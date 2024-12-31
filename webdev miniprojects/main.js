a5b571e52b957842ac5c3b567acb9338f3165e18
prabhav07022006 <prabhav722006@gmail.com>
2024-12-31 21:38:19 +0530

    added strucure and style to temp converter

---
+++ webdev miniprojects/main.js
@@ -0,0 +1,5 @@
+const option1 = document.getElementById("option1");
+const option2 = document.getElementById("option2");
+const myInput = document.getElementById("myInput");
+const message = document.getElementById("message");
+
