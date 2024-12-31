6d1416f145d9ecb3afea48ec2c2eabc640e5f799
prabhav07022006 <prabhav722006@gmail.com>
2024-12-31 21:56:34 +0530

    added functionality

--- webdev miniprojects/main.js
+++ webdev miniprojects/main.js
@@ -2,4 +2,19 @@ const option1 = document.getElementById("option1");
 const option2 = document.getElementById("option2");
 const myInput = document.getElementById("myInput");
 const message = document.getElementById("message");
+const mysubmit = document.getElementById("mySubmit");
 
+mysubmit.onclick = function(){
+    let a = myInput.value;
+    a=Number(a);
+
+    if(option1.checked){
+       let b = 5*(a-32)/9;
+        message.textContent = "The temperature in farhenheit is " + b + ".";
+    }else if(option2.checked){
+        b = ((9/5)*a) + 32;
+        message.textContent = "The temperature in celsius is " + b + ".";
+    }else{
+        alert("Choose a conversion option!")
+    }
+}
