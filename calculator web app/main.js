d0f92e494458a38a7dc232a67a6c0ba5210b58b1
prabhav07022006 <prabhav722006@gmail.com>
2025-01-03 16:46:15 +0530

    style and functionality added

---
+++ calculator web app/main.js
@@ -0,0 +1,6 @@
+const myButton1 = document.getElementById('myButton1');
+const myButton2 = document.getElementById('myButton2');
+const myButton3 = document.getElementById('myButton3');
+const myButtonadd = document.getElementById('myButton+');
+
+con
